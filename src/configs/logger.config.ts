7aa3fbf32c481a343539dc93a7de9aa811a9097c
The Berufegoru <giftmoobi@gmail.com>
2024-08-26 13:11:47 +0200

    initial update

--- src/configs/logger.config.ts
+++ src/configs/logger.config.ts
@@ -6,6 +6,7 @@
 import { ILoggerConfig } from '../interfaces';
 
 export const loggerConfig: ILoggerConfig = {
+  admin: 'LOGTAIL_ADMIN_TOKEN',
   auth: 'LOGTAIL_AUTH_TOKEN',
   system: 'LOGTAIL_SYSTEM_TOKEN',
 };
