5b06d84a77ea8dbd84dc54700632f18151bc0e7f
The Berufegoru <giftmoobi@gmail.com>
2024-08-27 10:27:42 +0200

    added recruiter logger

--- src/configs/logger.config.ts
+++ src/configs/logger.config.ts
@@ -8,5 +8,6 @@ import { ILoggerConfig } from '../interfaces';
 export const loggerConfig: ILoggerConfig = {
   admin: 'LOGTAIL_ADMIN_TOKEN',
   auth: 'LOGTAIL_AUTH_TOKEN',
+  recruiter: 'LOGTAIL_RECRUITER_TOKEN',
   system: 'LOGTAIL_SYSTEM_TOKEN',
 };
