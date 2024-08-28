118cb5614682669e7690d805583374bddfbc5044
The Berufegoru <giftmoobi@gmail.com>
2024-08-29 00:09:38 +0200

    added application dto

---
+++ src/dtos/application.dto.ts
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview
+ * @module
+ * @version
+ */
+
+import { IApplication } from '../interfaces';
+
+interface IApplicationDTO {
+  id: number;
+  job: {
+    id: number;
+    title: string | null;
+    recruiter: {
+      id: number;
+      name: string | null;
+    };
+  };
+  candidate: {
+    id: number;
+    firstName: string | null;
+    lastName: string | null;
+    user: {
+      avatarUrl: object;
+    };
+  };
+  status: string | null;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const toIApplicationDTO = (application: IApplication): IApplicationDTO => ({
+  id: application['id'],
+  job: {
+    id: application.job.id,
+    title: application.job.title,
+    recruiter: {
+      id: application.job.recruiter.id,
+      name: application.job.recruiter.name,
+    },
+  },
+  candidate: {
+    id: application.candidate.id,
+    firstName: application.candidate.firstName,
+    lastName: application.candidate.lastName,
+    user: {
+      avatarUrl: application.candidate.user.avatarUrl,
+    },
+  },
+  status: application['status'],
+});
+
+export { IApplicationDTO, toIApplicationDTO };
